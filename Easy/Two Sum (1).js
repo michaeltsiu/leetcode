ec7ccfc5e87607d1a85ecbe98445accf5f9161d3
Michael Siu <michaeltsiu13@gmail.com>
2020-10-08 11:13:26 -0700

    Two Sum completed answer

--- Easy/Two Sum (1).js
+++ Easy/Two Sum (1).js
@@ -18,11 +18,13 @@
 //  */
 
 var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-      for (let j = 0; j < nums.length; j++) {
-          if (nums[i] + nums[j] === target) {
-              return [i, j]
-          }
-      }
-  }
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if (i !== j) {
+                if (nums[i] + nums[j] == target) {
+                    return [i, j]
+                }
+            }
+        }
+    }
 };
