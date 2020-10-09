ce1d9f45c247d4be3a285aaf6ca6b92b551d5b79
Michael Siu <michaeltsiu13@gmail.com>
2020-10-09 13:04:35 -0700

    Reverse String

---
+++ Easy/Reverse String(344).js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/reverse-string/
+
+// Write a function that reverses a string. The input string is given as an array of characters char[].
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// You may assume all the characters consist of printable ascii characters.
+
+
+
+// Example 1:
+
+// Input: ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+// /**
+//  * @param {character[]} s
+//  * @return {void} Do not return anything, modify s in-place instead.
+//  */
+var reverseString = function(s) {
+  let first = 0; last = s.length - 1;
+  while (first < last) {
+      let temp = s[first];
+      s[first] = s[last];
+      s[last] = temp;
+      first++
+      last--
+  }
+};
