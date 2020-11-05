1a5752dda5be3bd45107749beb5575ad75c87106
Michael Siu <michaeltsiu13@gmail.com>
2020-11-05 12:23:47 -0800

    Power of Three

---
+++ Easy/Power of Three(326).js
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/power-of-three/
+
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == x3.
+
+
+
+// Example 1:
+
+// Input: n = 27
+// Output: true
+// Example 2:
+
+// Input: n = 0
+// Output: false
+// Example 3:
+
+// Input: n = 9
+// Output: true
+// Example 4:
+
+// Input: n = 45
+// Output: false
+
+
+// Constraints:
+
+// -231 <= n <= 231 - 1
+
+
+// Follow up: Could you do it without using any loop / recursion?
+
+// /**
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+var isPowerOfThree = function(n) {
+  if (n < 1) return false;
+  while (n % 3 === 0) n /= 3
+  return n == 1
+};
