fe03a8aba92ead1c234a342f2c119e79d0f6ad7e
Michael Siu <michaeltsiu13@gmail.com>
2020-11-06 14:34:13 -0800

    Merge Two Sorted Lists

--- Easy/Merge Two Sorted Lists(21).js
+++ Easy/Merge Two Sorted Lists(21).js
@@ -1,30 +1,55 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+// Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+// Example 1:
+// Input: l1 = [1,2,4], l2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+// Input: l1 = [], l2 = []
+// Output: []
+
+// Example 3:
+// Input: l1 = [], l2 = [0]
+// Output: [0]
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both l1 and l2 are sorted in non-decreasing order.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// /**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+
 var mergeTwoLists = function(l1, l2) {
-  let dummyHead = new ListNode();
-  let currentNode = dummyHead;
+  let list = new ListNode();
+  let head = list;
 
   while (l1 && l2) {
     if (l1.val < l2.val) {
-      currentNode = l1;
+      list.next = new ListNode(l1.val);
       l1 = l1.next;
     } else {
-      currentNode.next = l2;
+      list.next = new ListNode(l2.val);
       l2 = l2.next;
     }
-    currentNode = currentNode.next;
+    list = list.next;
   }
 
-  while (l1) {
-    currentNode.next = l1;
-    l1 = l1.next;
-    currentNode = currentNode.next;
-  }
-
-  while (l2) {
-    currentNode.next = l2;
-    l2 = l2.next;
-    currentNode =  currentNode.next;
-  }
-  return dummyHead.next;
+  if (l1) list.next = l1;
+  if (l2) list.next = l2;
+  return head.next;
 };
-
-console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]))
