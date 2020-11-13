d3786b280938d00a3e252753e36d7b2571dcc0a3
Michael Siu <michaeltsiu13@gmail.com>
2020-11-12 16:58:13 -0800

    Second variation of the productExceptSelf

--- Medium/Product of Array Except Self(238).js
+++ Medium/Product of Array Except Self(238).js
@@ -18,9 +18,13 @@
 //  * @return {number[]}
 //  */
 var productExceptSelf = function(nums) {
+  // create empty array to hold all products;
   let allProducts = [];
+  // create variable to hold the product
   let currentProduct = 1;
+  // iterate over the nums array
   for (let i = 0; i < nums.length; i++) {
+    // add values to
     allProducts[i] = currentProduct;
     currentProduct *= nums[i];
   }
@@ -29,5 +33,28 @@ var productExceptSelf = function(nums) {
     allProducts[i] *= currentProduct;
     currentProduct *= nums[i];
   }
+  // return all products;
   return allProducts;
 };
+
+// Second variation answer
+var productExceptSelf = function(nums) {
+  let N = nums.length;
+  let left_products = new Array(N);
+  let right_products = new Array(N);
+  let output_arr = new Array(N);
+
+  left_products[0] = 1;
+  right_products[N-1] = 1;
+
+  for (let i = 1; i < N; i++) {
+    left_products[i] = nums[i-1] * left_products[i-1];
+  }
+  for (let i = N-2; i >= 0; i--) {
+    right_products[i] = nums[i+1] * right_products[i+1];
+  }
+  for (let i = 0; i < N; i++) {
+    output_arr[i] = left_products[i] * right_products[i];
+  }
+  return output_arr;
+}
