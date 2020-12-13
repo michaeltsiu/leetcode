89b64491dd37e5b27a3c387a62051072cc094416
Michael Siu <michaeltsiu13@gmail.com>
2020-12-12 17:06:26 -0800

    Pseudocode for rotate list

--- Medium/Rotate List(61).js
+++ Medium/Rotate List(61).js
@@ -32,29 +32,47 @@
 //  */
 
 var rotateRight = function(head, k) {
+  // if the head is not present, return the head
   if (!head) return head;
+  // helper function to find the length of the list
   let length = lengthOfList(head);
+  // iterate over k modulo length so it doesn't run more than it needs to
   for (let i = 0; i < (k%length); i++) {
+    // create variables newNode to hold a node, set node and prev to be head;
     let newNode = new ListNode(), node = head, prev = head;
+    // while there is a next node
     while (node.next) {
+      // set prev to be node
       prev = node;
+      // set node to be the next
       node = node.next;
     }
+    // if the length is greater than 0
     if (length > 0) {
-        prev.next = null;
-        newNode.val = node.val;
-        newNode.next = head;
-        head = newNode;
+      // set the prev's next to be null, cutting the tail off
+      prev.next = null;
+      // set the newNode's value to be the node's val
+      newNode.val = node.val;
+      // set the newnode's next to be head
+      newNode.next = head;
+      // set the head to be newnode
+      head = newNode;
     }
   }
+  // return head
   return head;
 }
-
+// helper function to find the length of list
 let lengthOfList = head => {
+  // set node to be head and length to be 0
   let node = head, length = 0;
+  // while node is there
   while (node) {
-      node = node.next;
-      length++;
+    // set node to be the next node
+    node = node.next;
+    // increment length
+    length++;
   }
+  // return length
   return length;
 }
